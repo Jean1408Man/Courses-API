7077522d1769c67328e6dfa95fdb0dfba2e765cb
jean.mmp <jean.mmp@avangenio.com>
2025-04-03 20:41:53 -0400

    feat(schemas): add new user interface schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -21,3 +21,11 @@ class UserRead(UserBase):
 
     class Config:
         orm_mode = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class LoginData(BaseModel):
+    email: str
+    password: str
